c6fe2f5986fefabd9237a979ef4e2f4b354355c9
EmanuelFerreira <48385634+Bebel132@users.noreply.github.com>
2026-09-15 12:04:29 -0300

    adiciona quadrinho 4

---
+++ quadrinhos/04/script.js
@@ -0,0 +1,31 @@
+const gato = document.querySelector("#gato");
+const gato2 = document.querySelector("#gato1_2");
+const quadros_final_ruim = document.querySelectorAll(".caminho_ruim");
+const quadros_final_bom = document.querySelectorAll(".caminho_bom");
+const miau = new Audio("miado.mp3");
+miau.preload = 'auto';
+miau.volume = 0.7;
+miau.loop = false;
+
+const scrollEvent = () => {
+    if (window.scrollY > gato.parentElement.parentElement.children[2].offsetTop - 100) {
+        gato2.style.display = "block";
+        gato2.addEventListener("click", () => {
+            gato2.style.display = "none";
+            gato.click()
+        });
+    }
+}
+
+const clickEvent = () => {
+    window.removeEventListener("scroll", scrollEvent);
+    window.scrollTo({
+        top: gato.parentElement.parentElement.children[2].offsetTop + 50,
+        behavior: "smooth"
+    });
+    quadros_final_ruim.forEach(e => e.style.display = "none");
+    quadros_final_bom.forEach(e => e.style.display = "block");
+}
+
+gato.addEventListener("click", clickEvent);
+window.addEventListener("scroll", scrollEvent);
