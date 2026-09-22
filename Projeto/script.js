bef0bbd31caefe7b692538b4396d9bacc6ade07e
Emanuel <emanuel2005batista@gmail.com>
2026-09-22 19:08:24 -0300

    adiciona animações no quadrinho principal e ajusta detalhes de responsividade

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,18 +1,28 @@
 let chegouAoFinal = false;
+let iniciaAnimacaoPagina3 = false;
 
 window.addEventListener("scroll", () => {
-    // if (window.innerWidth > 768) {
-    //     chegouAoFinal = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 200;
-    // } else {
-    //     chegouAoFinal = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 300;
-    // }
+    if(
+        window.innerHeight + window.scrollY >= document.documentElement.scrollHeight * 0.83
+        && !iniciaAnimacaoPagina3
+    ){
+        iniciaAnimacaoPagina3 = true;
+        const chocalhos = document.querySelectorAll(".chocalho");
+        
+        chocalhos[0].style.display = "none";
+        
+        setTimeout(() => {
+            chocalhos[0].style.display = "block";
+            chocalhos[1].style.display = "none";
+        }, 1800)
+        chocalhos[1].style.display = "block";
+    }
 
     if(!chegouAoFinal){
-    chegouAoFinal = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight * 0.85;
+        chegouAoFinal = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight * 0.95;
     }
 
     if(chegouAoFinal){
-
         document.querySelectorAll(".patrão").forEach(x => {
             x.style.display = "none";
         }) 
@@ -23,3 +33,4 @@ window.addEventListener("scroll", () => {
 
     }
 })
+
