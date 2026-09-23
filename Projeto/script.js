45e39f94000a4213fbc3dd71a62f541efe958987
Emanuel <emanuel2005batista@gmail.com>
2026-09-23 10:41:49 -0300

    corrige bugs

--- Projeto/script.js
+++ Projeto/script.js
@@ -4,63 +4,61 @@ let iniciaAnimacaoPagina3 = false;
 let coletável = 0;
 
 window.addEventListener("scroll", () => {
-    if(
-        window.innerHeight + window.scrollY >= document.documentElement.scrollHeight * 0.83
-        && !iniciaAnimacaoPagina3
-    ){
-        iniciaAnimacaoPagina3 = true;
-        const chocalhos = document.querySelectorAll(".chocalho");
-        
-        chocalhos[0].style.display = "none";
-        
-        setTimeout(() => {
-            chocalhos[0].style.display = "block";
-            chocalhos[1].style.display = "none";
-        }, 1800)
-        chocalhos[1].style.display = "block";
-    }
+    if(comicDestravada){
+        if(
+            window.innerHeight + window.scrollY >= document.documentElement.scrollHeight * 0.83
+            && !iniciaAnimacaoPagina3
+        ){
+            iniciaAnimacaoPagina3 = true;
+            const chocalhos = document.querySelectorAll(".chocalho")
 
-    if(!chegouAoFinal){
-        chegouAoFinal = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight * 0.95;
-    }
-
-    if(chegouAoFinal && comicDestravada){
-
-        document.querySelectorAll(".patrão").forEach(x => {
-            x.style.display = "none";
-        }) 
-
-        document.querySelectorAll(".ocultDay").forEach(x => {
-            x.style.display = "block";
-        })
-
+            chocalhos[0].style.display = "none";
+            setTimeout(() => {
+                chocalhos[0].style.display = "block";
+                chocalhos[1].style.display = "none";
+            }, 2300);
+        }
+    
+        if(!chegouAoFinal){
+            chegouAoFinal = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight * 0.95;
+        }
+    
+        if(chegouAoFinal && comicDestravada){
+    
+            document.querySelectorAll(".patrão").forEach(x => {
+                x.style.display = "none";
+            }) 
+    
+            document.querySelectorAll(".ocultDay").forEach(x => {
+                x.style.display = "block";
+            })
+        }
     }
 })
 
 document.querySelectorAll(".coletável").forEach(x => {
-            x.addEventListener("click", (e) => {
+    x.addEventListener("click", (e) => {
+        x.style.display = "none";
+        coletável += 1;
+
+        if(coletável>=3){
+            comicDestravada = true;
+        }
+
+        if(comicDestravada){
+            
+            document.querySelectorAll(".locked").forEach(x => {
                 x.style.display = "none";
-                coletável += 1;
+            })
+            
+            
+            document.querySelectorAll(".unlocked").forEach(x => {
+                x.style.display = "block";
+            })
 
-                if(coletável>=3){
-                    comicDestravada = true;
-                    chegouAoFinal = false
-                }
-
-                if(comicDestravada){
-                    
-                    document.querySelectorAll(".locked").forEach(x => {
-                        x.style.display = "none";
-                    })
-                    
-                    
-                    document.querySelectorAll(".unlocked").forEach(x => {
-                        x.style.display = "block";
-                    })
-
-                    document.querySelectorAll(".patrão").forEach(x => {
-                        x.style.display = "block";
-                    })
-                }
-            });
-        })
+            document.querySelectorAll(".patrão").forEach(x => {
+                x.style.display = "block";
+            })
+        }
+    });
+})
