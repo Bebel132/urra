8f67eba75c22890dbddafdba8a685e85f4b65cf8
Emanuel <emanuel2005batista@gmail.com>
2026-09-23 22:28:23 -0300

    formata o código com o prettier

--- Projeto/script.js
+++ Projeto/script.js
@@ -4,13 +4,14 @@ let iniciaAnimacaoPagina3 = false;
 let coletavel = 0;
 
 window.addEventListener("scroll", () => {
-    if(comicDestravada){
-        if(
-            window.innerHeight + window.scrollY >= document.documentElement.scrollHeight * 0.83
-            && !iniciaAnimacaoPagina3
-        ){
+    if (comicDestravada) {
+        if (
+            window.innerHeight + window.scrollY >=
+                document.documentElement.scrollHeight * 0.83 &&
+            !iniciaAnimacaoPagina3
+        ) {
             iniciaAnimacaoPagina3 = true;
-            const chocalhos = document.querySelectorAll(".chocalho")
+            const chocalhos = document.querySelectorAll(".chocalho");
 
             chocalhos[0].style.display = "none";
             setTimeout(() => {
@@ -18,50 +19,46 @@ window.addEventListener("scroll", () => {
                 chocalhos[2].style.display = "none";
             }, 2300);
         }
-    
-        if(!chegouAoFinal){
-            chegouAoFinal = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight * 0.95;
+
+        if (!chegouAoFinal) {
+            chegouAoFinal =
+                window.innerHeight + window.scrollY >=
+                document.documentElement.scrollHeight * 0.95;
         }
-    
-        if(chegouAoFinal && comicDestravada){
-    
-            document.querySelectorAll(".patrao").forEach(x => {
+
+        if (chegouAoFinal && comicDestravada) {
+            document.querySelectorAll(".patrao").forEach((x) => {
                 x.style.display = "none";
-            }) 
-    
-            document.querySelectorAll(".ocultDay").forEach(x => {
+            });
+
+            document.querySelectorAll(".ocultDay").forEach((x) => {
                 x.style.display = "block";
-            })
+            });
         }
     }
-})
+});
 
-document.querySelectorAll(".coletavel").forEach(x => {
+document.querySelectorAll(".coletavel").forEach((x) => {
     x.addEventListener("click", (e) => {
-
-
-
         x.style.display = "none";
         coletavel += 1;
 
-        if(coletavel>=3){
+        if (coletavel >= 3) {
             comicDestravada = true;
         }
 
-        if(comicDestravada){
-            
-            document.querySelectorAll(".locked").forEach(x => {
+        if (comicDestravada) {
+            document.querySelectorAll(".locked").forEach((x) => {
                 x.style.display = "none";
-            })
-            
-            
-            document.querySelectorAll(".unlocked").forEach(x => {
-                x.style.display = "block";
-            })
+            });
 
-            document.querySelectorAll(".patrão").forEach(x => {
+            document.querySelectorAll(".unlocked").forEach((x) => {
                 x.style.display = "block";
-            })
+            });
+
+            document.querySelectorAll(".patrão").forEach((x) => {
+                x.style.display = "block";
+            });
         }
     });
-})
+});
