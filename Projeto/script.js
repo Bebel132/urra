dc7072f2ce000295d8e3c3d9dbd14380949a87ad
Emanuel <emanuel2005batista@gmail.com>
2026-09-23 22:47:40 -0300

    adiciona navegação para os protótipos

--- Projeto/script.js
+++ Projeto/script.js
@@ -62,3 +62,12 @@ document.querySelectorAll(".coletavel").forEach((x) => {
         }
     });
 });
+
+
+document.querySelector("#botao-menu").addEventListener("click", () => {
+    document.querySelector("#botao-prototipos").classList.toggle("unlocked");
+});
+
+document.querySelector("#botao-prototipos").addEventListener("click", () => {
+    window.location.href = "Prototipos/index.html";
+});
